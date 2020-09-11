d4444a0fcca9b6e78e462c0d2262bcd2cd7705e7
Yanuar Aditia <yanuaraditia@outlook.com>
2020-09-12 05:40:56 +0700

    Preserve

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@
         <b-navbar-brand to="/" class="mr-md-5 pr-md-3"><g-image src="~/assets/images/logo-large.png" width="105px"></g-image></b-navbar-brand>
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
         <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav class="d-flex justify-content-between">
+          <b-navbar-nav class="d-md-flex justify-content-md-between px-3 py-2 mt-2 mt-md-0 p-md-0">
               <b-nav-item to="/anggota">Anggota</b-nav-item>
               <b-nav-item to="/covid">Covid-19</b-nav-item>
               <b-nav-item to="/kuisioner">Kuisioner</b-nav-item>
