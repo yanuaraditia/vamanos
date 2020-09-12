7ed2e3e3a14932a2b5079cad879f747a5e4b53c3
Yanuar Aditia <yanuaraditia@outlook.com>
2020-09-12 17:27:00 +0700

    TEr

--- src/pages/Covid.vue
+++ src/pages/Covid.vue
@@ -4,7 +4,7 @@
             <div class="container pt-lg-5">
                 <div class="row">
                     <div class="col-md-6 pb-lg-5">
-                        <h1 class="ndes-1 f-2">Jaga selalu kesehatanmu, ada mimpi yang harus kau kejar</h1>
+                        <h1 class="ndes-1 f-2"><span class="text-danger">{{caseTotal}}</span> kasus positif di sekitar kita, patuhi selalu protokol kesehatan.</h1>
                         <p class="mb-lg-4">Selalu mencuci tangan, kenakan masker, jaga jarak minimal 1 meter. Kurangi aktifitas di ruang publik, stay safe stay alive.</p>
                     </div>
                     <div class="col-md-6 text-center d-lg-flex align-items-lg-end justify-content-lg-end">
@@ -15,29 +15,17 @@
         </section>
         <section class="py-5 bg-dark text-white">
             <div class="container">
-                <div class="row">
-                    <div class="col-md-6 col-12">
-                        <h6>Jawa Tengah</h6>
-                        <h1 class="ndes-1 f-2 text-muted">
-                            <div v-if="isLoaded">
-                                <span class="text-danger">{{jateng.counter}}</span> /
-                                <span class="text-success">{{jateng.recover}}</span>
-                            </div>
-                            <b-spinner v-else label="Loading..."></b-spinner>
-                        </h1>
-                    </div>
-                    <div class="col-md-6 col-12">
-                        <h6>Daerah Istimewa Yogyakarta</h6>
-                        <h1 class="ndes-1 f-2 text-muted">
-                            <div v-if="isLoaded">
-                                <span class="text-danger">{{jogja.counter}}</span> /
-                                <span class="text-success">{{jogja.recover}}</span>
-                            </div>
-                            <b-spinner v-else label="Loading..."></b-spinner>
-                        </h1>
+                <div class="row" v-if="isLoaded">
+                    <div class="col-md-4 col-12 mb-4" v-for="sebaran in dataMentah" :key="sebaran.fid" v-bind:class="sebaran.fid == 35 ? 'd-none' : null">
+                        <h6>{{sebaran.provinsi}}</h6>
+                        <h3 class="f-2 text-muted">
+                            <span class="text-danger">{{sebaran.kasusPosi}}</span> /
+                            <span class="text-success">{{sebaran.kasusSemb}}</span> /
+                            <span class="text-muted">{{sebaran.kasusMeni}}</span>
+                        </h3>
                     </div>
                 </div>
-                <p class="text-muted mt-2">Data dari <a href="//covid19.go.id">Gugus Tugas Covid-19</a>. Muat ulang setiap 5 menit jika tidak dilakukan refresh.</p>
+                <p class="text-muted mt-2">Merah = positif, Hijau = sembuh, Kelabu = meninggal.Data dari <a href="//covid19.go.id">Gugus Tugas Covid-19</a>. Muat ulang setiap 5 menit jika tidak dilakukan refresh.</p>
             </div>
         </section>
     </Layout>
@@ -48,14 +36,6 @@ export default {
     data() {
         return {
             isLoaded: false,
-            jateng: {
-                counter: 0,
-                recover: 0,
-            },
-            jogja: {
-                counter: 0,
-                revover: 0,
-            },
             dataMentah: []
         }
     },
@@ -65,15 +45,6 @@ export default {
             .get('https://dev.imaka.or.id/api/deployer')
             .then(res => {
                 this.dataMentah = res.data.data
-                for(var i = 0; i< this.dataMentah.length ; i++) {
-                    if(this.dataMentah[i]['kodeProvi'] == 33) {
-                        this.jateng.counter = this.dataMentah[i]['kasusPosi']
-                        this.jateng.recover = this.dataMentah[i]['kasusSemb']
-                    } else if(this.dataMentah[i]['kodeProvi'] == 34) {
-                        this.jogja.counter = this.dataMentah[i]['kasusPosi']
-                        this.jogja.recover = this.dataMentah[i]['kasusSemb']
-                    }
-                }
                 this.isLoaded = true
             }).catch(e => {
                 alert('Terjadi kesalahan')
@@ -87,6 +58,15 @@ export default {
             this.isLoaded = false
             this.loadData()
         },300000)
+    },
+    computed: {
+        caseTotal() {
+            var total = 0;
+            for(var i = 0; i < this.dataMentah.length; i++) {
+                total += this.dataMentah[i]['kasusPosi']
+            }
+            return total;
+        }
     }
 }
 </script>
